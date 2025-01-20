7f0f09f2bee30891f4f25f4cfbbb2c85f3921e86
LIANAKES <lianakes@gmail.com>
2025-01-20 21:25:38 +0100

    Lesson_03

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,30 @@
 import "./App.css";
-import Homework02 from './homeworks/Homework02/Homework02';
+import Homework02Teacher from "./homeworks/Homework02Teacher/Homework02Teacher";
 //импорт по умолчанию (название компонента можно заменить на любое другое)
 // import MyButton from "./components/Button/Button";
-// import Button from "./components_Button/Button";
-function App() {
- return (
- <div className="app">
-   {/* <Button/>
-   // <Button/>
-  //  <Button/>*/}
-  <Homework02 />
-</div>
-);
-}
+// import Button from "./components/Button/Button";
 
+//lessons
+// import Lesson02 from "./lessons/Lesson02/Lesson02";
+//import Lesson03 from "./lessons/Lesson_03/Lesson03";
+
+//homeworks
+// import Homework02 from "./homeworks/Homework02/Homework02";
+
+function App() {
+  return (
+    <div className="app">
+      {/* <Button />
+      <Button />
+      <Button /> */}
+      {/* ---Topic: Components */}
+      {/* <Lesson02/> */}
+      {/* <Homework02/> */}
+      {/* ---Topic: Props */}
+      {/*<Lesson03 />*/}
+      <Homework02Teacher/>
+    </div>
+  );
+}
 
 export default App;
---
+++ src/homeworks/Homework02Teacher/Homework02Teacher.jsx
@@ -0,0 +1,16 @@
+import "./styles.css";
+
+import ProfileCardTeacher from "../../components/ProfileCardTeacher/ProfileCardTeacher";
+
+
+function Homework02Teacher() {
+  return (
+    <div className="homework02-wrapper">
+      <ProfileCardTeacher />
+      <ProfileCardTeacher />
+      <ProfileCardTeacher />
+    </div>
+  );
+}
+
+export default Homework02Teacher;
