36a3349678550deabbc61b9fbb732539be4cf512
LIANAKES <lianakes@gmail.com>
2025-01-22 10:07:31 +0100

    Homework_04

---
+++ src/homeworks/Homework04/Homework04.jsx
@@ -0,0 +1,14 @@
+
+import Feedback from "../../components/Feedback/Feedback";
+import "./Homework04.css"
+
+const Homework04 = () => {
+    return (
+      <div className="homeworkContainer">
+        <h1 className="homeworkTitle">Homework 04: Feedback Component</h1>
+        <Feedback />
+      </div>
+    );
+  };
+  
+  export default Homework04;
