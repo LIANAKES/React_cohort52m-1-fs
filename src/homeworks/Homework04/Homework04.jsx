eb0ec7a9ca669aea1b7cf2a224dcba0eeb5ac576
LIANAKES <lianakes@gmail.com>
2025-01-23 09:47:54 +0100

    Update Homework04.jsx

--- src/homeworks/Homework04/Homework04.jsx
+++ src/homeworks/Homework04/Homework04.jsx
@@ -2,7 +2,7 @@
 import Feedback from "../../components/Feedback/Feedback";
 import "./Homework04.css"
 
-const Homework04 = () => {
+function Homework04 = () => {
     return (
       <div className="homeworkContainer">
         <h1 className="homeworkTitle">Homework 04: Feedback Component</h1>
